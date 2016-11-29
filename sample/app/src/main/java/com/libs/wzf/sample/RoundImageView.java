98c4216f28809de255a413fbc76602493675a8bf
Peter Wang <wangzf0342@qq.com>
2016-11-29 11:20:44 +0800

    增加一个的sample

---
+++ sample/app/src/main/java/com/libs/wzf/sample/RoundImageView.java
@@ -0,0 +1,65 @@
+package com.libs.wzf.sample;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * ================================================
+ * 描    述：
+ * 作    者：王智凡
+ * 创建日期：2016/11/29
+ * 版    本：1.0
+ * 修订历史：
+ * ================================================
+ */
+public class RoundImageView extends ImageView {
+
+    private Paint mPaint, mStrokePaint, mPathPaint;
+
+    public RoundImageView(Context context) {
+        super(context);
+    }
+
+    public RoundImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RoundImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        setLayerType(LAYER_TYPE_HARDWARE, null);
+        mPaint = new Paint();
+        mPaint.setFilterBitmap(true);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+
+        mStrokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mStrokePaint.setFilterBitmap(true);
+        mStrokePaint.setColor(Color.RED);
+
+        mPathPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPathPaint.setFilterBitmap(true);
+        mPathPaint.setColor(Color.BLACK);
+        mPathPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_test);
+    }
+}
